06d2aaae8474754017562dfaaf9915bae61e8b61
ihtnc <ihopethisnamecounts@yahoo.com>
2024-05-27 18:13:00 +1000

    Updated tank game link
    * Removed tank game demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
-          href="/apps/tank-game/index.html"
+          href="https://ihtnc.github.io/canvas-concoctions/concoctions/tank-game?no-nav&no-title&no-padding"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
