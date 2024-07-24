f9a474af2a252d01ba2e2a73e925d3285f7695fb
ihtnc <ihopethisnamecounts@yahoo.com>
2024-07-24 23:36:20 +1000

    Added use-animated-canvas project
    * Refactored project listing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,20 @@
 export default function Home() {
+  type ProjectDetails = {
+    name: string,
+    uri: string,
+    description: string
+  }
+
+  const projects: Array<ProjectDetails> = [{
+    name: "Tank Game",
+    uri: "https://ihtnc.github.io/canvas-concoctions/concoctions/tank-game?no-nav&no-title&no-padding",
+    description: "A tank game made with the help of GitHub Copilot."
+  },{
+    name: "use-animated-canvas",
+    uri: "https://www.npmjs.com/package/@ihtnc/use-animated-canvas",
+    description: "A React hook to create a canvas element with a built-in render loop."
+  }]
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -14,22 +30,24 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://ihtnc.github.io/canvas-concoctions/concoctions/tank-game?no-nav&no-title&no-padding"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Tank Game{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            A tank game made with the help of GitHub Copilot.
-          </p>
-        </a>
+        {projects.map((project, index) => (
+          <a key={index}
+            href={project.uri}
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className="mb-3 text-2xl font-semibold">
+              {`${project.name} `}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+              {project.description}
+            </p>
+          </a>
+        ))}
       </div>
     </main>
   );
